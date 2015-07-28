51419e245d08fe865b65be39b6099f538b590b99
rudzaytsev <rudzaytsev@gmail.com>
2015-07-29 01:05:07 +0300

    #21 Refactoring station service.

--- src/main/java/com/tsystems/jschool/railage/service/StationService.java
+++ src/main/java/com/tsystems/jschool/railage/service/StationService.java
@@ -6,6 +6,7 @@ import com.tsystems.jschool.railage.service.exceptions.DomainObjectAlreadyExists
 import com.tsystems.jschool.railage.service.exceptions.IncorrectParameterException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -20,43 +21,29 @@ public class StationService {
     @Autowired
     private StationDao stationDao;
 
+    private static final String STATION_NAME_PATTERN = "^[0-9a-zA-Z]+";
+
     public List<Station> findAllStations(){
         return stationDao.findAll();
     }
 
     public Station findStationById(Integer id){
-        Station station;
-        //stationDao.open();
-        try {
-            station = stationDao.findById(id);
-        }
-        finally {
-            //stationDao.close();
-        }
-        return station;
+        return stationDao.findById(id);
     }
 
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
     public void addStation(String stationName)
             throws DomainObjectAlreadyExistsException, IncorrectParameterException {
 
-       if (!stationName.matches("^[0-9a-zA-Z]+")){
+       if (!stationName.matches(STATION_NAME_PATTERN)){
             throw new IncorrectParameterException(
                     "Station name should contains only latin characters and digits");
        }
-
-       //stationDao.open();
-       try {
-           List<Station> stations = stationDao.findStationsByName(stationName);
-           if (!stations.isEmpty()) {
-               throw new DomainObjectAlreadyExistsException(
-                       "Station with such parameters already exists");
-           }
-           else {
-              stationDao.persist(new Station(stationName));
-           }
+       List<Station> stations = stationDao.findStationsByName(stationName);
+       if (!stations.isEmpty()) {
+           throw new DomainObjectAlreadyExistsException(
+                   "Station with such parameters already exists");
        }
-       finally {
-           //stationDao.close();
-       }
+       stationDao.persist(new Station(stationName));
     }
 }
